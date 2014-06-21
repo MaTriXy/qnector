7caa81970b1918d447ae817f39f0a897f46e3f8b
aashish9patel <aashish9patel@gmail.com>
2014-06-21 07:08:49 -0700

    Workspace + Icon

---
+++ src/com/qualcomm/qnector/ActiveCanvas.java
@@ -0,0 +1,75 @@
+package com.qualcomm.qnector;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+ 
+public class ActiveCanvas extends SurfaceView implements SurfaceHolder.Callback {
+    //private ArrayList<Reactor> reactors = new ArrayList<Reactor>();
+    private Paint paint;
+    
+    public ActiveCanvas(Context context) {
+        super(context);
+        this.getHolder().addCallback(this);
+        setFocusable(true);
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+    }
+ 
+    @Override
+    protected void onDraw(Canvas canvas) {
+/*        for(Reactor r: reactors)
+        	canvas.drawBitmap(r.getReactor(), r.getX() - (r.getWidth() / 2), r.getY() - (r.getHeight() / 2), null);
+        for(int i = 0; i < lines.size()/2; i+=2){
+        	canvas.drawLine(reactors.get(lines.get(i)).getX(), reactors.get(lines.get(i)).getY(), 
+        			reactors.get(lines.get(i+1)).getX(), reactors.get(lines.get(i+1)).getY(), paint);
+        }*/
+    }
+ 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {    
+    	if((int)event.getY() > 494)
+    		return true;
+    	
+/*        for(Reactor r: reactors){
+        	if(r.isSelected((int) event.getX(), (int) event.getY())){
+        		r.setX((int)event.getX());
+        		r.setY((int)event.getY());
+        		this.postInvalidate();
+        		rTouched = r.getId();
+        		break;
+        	}
+        }
+        if(rTouched == -1){
+        	sscoord[0] = (int) event.getX();
+        	sscoord[1] = (int) event.getY();
+        }
+        if(lastPressed[0] != rTouched){
+        	lastPressed[1] = lastPressed[0]; //FROM
+        	lastPressed[0] = rTouched;		// TO
+        }
+*/
+    	return true;
+    }
+ 
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+ 
+    }
+ 
+    public void surfaceCreated(SurfaceHolder holder) {
+        setWillNotDraw(false);
+    }
+ 
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+    
+}
--- src/com/qualcomm/qnector/QNector.java
+++ src/com/qualcomm/qnector/QNector.java
@@ -1,5 +1,4 @@
 package com.qualcomm.qnector;
-
 import java.io.File;
 import java.io.FilenameFilter;
 
@@ -11,72 +10,83 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class QNector extends Activity {
 	private static String TAG = "QNector.java";
 	private static AlertDialog.Builder builder;
     private String[] fileList;
     private File selectedFile;
+    
+	private Context context;
+	private ActiveCanvas wksp;
 	
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_qnector);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_qnector);
+        context = this;
+        wksp = new ActiveCanvas(this);
 
-                showFileSelectorDialog(this);
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-                return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-                return super.onOptionsItemSelected(item);
-        }
-        
-    	public Dialog showFileSelectorDialog(Context context) {
-    		File path = new File(Environment.getExternalStorageDirectory() + "/qnector/");
-    		try {
-    			path.mkdirs();
-    		} catch(SecurityException e) {
-    			Log.e(TAG, e.toString());
-    		}
-    		if(path.exists()) {
-    			FilenameFilter filter = new FilenameFilter() {
-    				public boolean accept(File dir, String filename) {
-    					File select = new File(dir, filename);
-    					return filename.contains(".sch") || select.isDirectory();
-    				}
-    			};
-    			fileList = path.list(filter);
-    		}
-    		else {
-    			fileList= new String[0];
-    		}
-    		
-    		Dialog dialog;
-    		builder = new AlertDialog.Builder(context);
-    		builder.setTitle("Select Schematic");
-    		if(fileList == null) {
-    			dialog = builder.create();
-    			return dialog;
-    		}
-    		builder.setItems(fileList, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    selectedFile = new File(fileList[which]);
-                    updateSchematic();
-                }
-            });
-    		dialog = builder.show();
-    		return dialog;
-    	}
-    	
-    	private void updateSchematic(){
-    		//TODO on dialog selection
-    	}
-
+        ImageButton openBttn = (ImageButton) findViewById(R.id.openBttn);
+        openBttn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	showFileSelectorDialog(context);
+            }
+        });
+        ImageButton goBttn = (ImageButton) findViewById(R.id.goBttn);
+        goBttn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	//TODO
+            }
+        });
+	}
+	
+	public Dialog showFileSelectorDialog(Context context) {
+		File path = new File(Environment.getExternalStorageDirectory() + "/qnector/");
+		try {
+			path.mkdirs();
+		} catch(SecurityException e) {
+			Log.e(TAG, e.toString());
+		}
+		if(path.exists()) {
+			FilenameFilter filter = new FilenameFilter() {
+				public boolean accept(File dir, String filename) {
+					File select = new File(dir, filename);
+					return filename.contains(".sch") || select.isDirectory();
+				}
+			};
+			fileList = path.list(filter);
+		}
+		else {
+			fileList= new String[0];
+		}
+		
+		Dialog dialog;
+		builder = new AlertDialog.Builder(context);
+		builder.setTitle("Select Schematic");
+		if(fileList == null) {
+			dialog = builder.create();
+			return dialog;
+		}
+		builder.setItems(fileList, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                selectedFile = new File(fileList[which]);
+                updateSchematic();
+            }
+        });
+		dialog = builder.show();
+		return dialog;
+	}
+	
+	private void updateSchematic(){
+		//TODO on dialog selection
+	}
 }
