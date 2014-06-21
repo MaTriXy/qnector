d9358674574d48f28bf6cd794eea14f19c508e9d
aashish9patel <aashish9patel@gmail.com>
2014-06-20 23:42:48 -0700

    FileSelector Functioning

--- src/com/qualcomm/qnector/QNector.java
+++ src/com/qualcomm/qnector/QNector.java
@@ -1,32 +1,36 @@
 package com.qualcomm.qnector;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Canvas;
+import android.content.DialogInterface;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class QNector extends Activity {
-	private static final String TAG = "CameraDemo";
+	private final String TAG = "QNector.java";
 	Camera camera;
 	Preview preview;
 	Button buttonClick;
+	
+	private static AlertDialog.Builder builder;
+	private String[] fileList;
+	private File selectedFile;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -34,6 +38,8 @@ public class QNector extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_qnector);
 
+		//showFileSelectorDialog(this);
+		
 		preview = new Preview(this);
 		((FrameLayout) findViewById(R.id.preview)).addView(preview);
 
@@ -85,5 +91,46 @@ public class QNector extends Activity {
 			Log.d(TAG, "onPictureTaken - jpeg");
 		}
 	};
+	
+	public Dialog showFileSelectorDialog(Context context) {
+		File path = new File(Environment.getExternalStorageDirectory() + "/qnector/");
+		try {
+			path.mkdirs();
+		} catch(SecurityException e) {
+			Log.e(TAG, e.toString());
+		}
+		if(path.exists()) {
+			FilenameFilter filter = new FilenameFilter() {
+				public boolean accept(File dir, String filename) {
+					File select = new File(dir, filename);
+					return filename.contains(".sch") || select.isDirectory();
+				}
+			};
+			fileList = path.list(filter);
+		}
+		else {
+			fileList= new String[0];
+		}
+		
+		Dialog dialog;
+		builder = new AlertDialog.Builder(context);
+		builder.setTitle("Select Schematic");
+		if(fileList == null) {
+			dialog = builder.create();
+			return dialog;
+		}
+		builder.setItems(fileList, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                selectedFile = new File(fileList[which]);
+                updateSchematic();
+            }
+        });
+		dialog = builder.show();
+		return dialog;
+	}
+	
+	private void updateSchematic(){
+		//TODO on dialog selection
+	}
 
 }
